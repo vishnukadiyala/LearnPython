56cd3331c4bee2993c05fdd98e6ed859deca8098
Vishnu Kadiyala <vishnupk@ou.edu>
2024-02-09 19:50:32 -0600

    Class 1 update

--- Class1.py
+++ Class1.py
@@ -1,2 +1,16 @@
+# STRINGS 
 
- 
+# Variable Assignment 
+# "" -> empty string
+# " " -> white space
+
+# Use '#' to Comment in python 
+
+name = "your name "
+lastname = "your lastname"
+
+print((name + lastname).title())
+
+
+
+
